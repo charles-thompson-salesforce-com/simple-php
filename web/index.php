9c35bf2d37f2b22c0fbf56d31b8b5bd265bda911
Charles Thompson <33275665+charles-thompson-salesforce-com@users.noreply.github.com>
2020-09-25 16:10:22 +1200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -3,7 +3,7 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?>
+ <?php echo '<h1>Hello World</h1>'; ?>
   <p>
   <a href='database.php'>Go To Database</a>
   </p>
